22986b1e27b24ac8849a00772290b7208e9dad90
Plaids <cfaulkn6@gmail.com>
2021-04-05 11:20:23 -0400

    solution

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function (array) {
+  let newArray = [];
+  array.map((item) => {
+    newArray.push(item.title);
+  });
+  return newArray;
+};
 
 module.exports = getTheTitles;
