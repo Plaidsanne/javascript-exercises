e00deb09cb62a3ea79d9d2192ee0f6c3c730cf8f
Plaids <cfaulkn6@gmail.com>
2021-04-05 12:04:30 -0400

    solution

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-let findTheOldest = function() {
+let findTheOldest = function (array) {
+  let oldest;
+  let currentDate = parseInt(new Date().getFullYear());
 
-}
+  oldest = array.reduce((accumulator, currentValue) => {
+    let accumAge =
+      (accumulator.yearOfDeath === undefined
+        ? currentDate
+        : accumulator.yearOfDeath) - accumulator.yearOfBirth;
+    let currValueAge =
+      (currentValue.yearOfDeath === undefined
+        ? currentDate
+        : currentValue.yearOfDeath) - currentValue.yearOfBirth;
+    if (accumAge > currValueAge) {
+      return accumulator;
+    } else {
+      return currentValue;
+    }
+  });
+  console.log(oldest);
+  return oldest;
+};
 
-module.exports = findTheOldest
+module.exports = findTheOldest;
