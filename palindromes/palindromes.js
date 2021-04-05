28ed3dee7af91a32f5f5fc3e6c6b3bd31cd5fd02
Plaids <cfaulkn6@gmail.com>
2021-04-05 10:16:59 -0400

    my solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function (s) {
+  let removePunc = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  let removeSpaces = removePunc.toLowerCase().replace(/\s+/g, "");
+  let original = removeSpaces.toLowerCase();
+  let reverseString = removeSpaces.split("").reverse().join("");
+  if (original === reverseString) {
+    console.log(original, reverseString);
+    return true;
+  }
+  return false;
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
