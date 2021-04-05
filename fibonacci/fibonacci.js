310db10467659dd79b36ba2a947d4dffab2fc93a
Plaids <cfaulkn6@gmail.com>
2021-04-05 11:02:30 -0400

    my solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (Math.sign(number) === -1) return "OOPS";
+  let newNumber = number;
+  if (typeof number === "string") {
+    newNumber = parseInt(number);
+  }
+  let sum = 0;
+  let previousValue = 0;
+  let array = [1];
+  for (i = 1; i <= newNumber; i++) {
+    sum = array[i - 1] + previousValue;
+    array.push(sum);
+    previousValue = array[i - 1];
+  }
+  let answer = array[newNumber - 1];
+  return answer;
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
